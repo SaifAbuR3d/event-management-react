155590c52a3e3cb8ef0db454547bdd8542762e8b
Sameeh Hussein <sameehhussein02@gmail.com>
2024-04-21 00:45:42 +0300

    Update Event Card Component

--- src/components/cards/EventCard.jsx
+++ src/components/cards/EventCard.jsx
@@ -5,29 +5,44 @@ import CardContent from "@mui/material/CardContent";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import FavoriteIcon from "@mui/icons-material/Favorite";
-import Sideimage1 from "../../assets/images/registerImges/Sideimage1.jpg";
-import { Box, Paper } from "@mui/material";
-import { IosShare, PeopleOutlineTwoTone } from "@mui/icons-material";
+import { Box, Paper, Snackbar } from "@mui/material";
+import {
+  DoneAll,
+  IosShare,
+  Key,
+  PeopleOutlineTwoTone,
+} from "@mui/icons-material";
+import { useState } from "react";
 
 export default function EventCard({
   name,
+  id,
   isOnline,
   startDate,
   startTime,
   organizerName,
   numberOfFollers,
+  imageUrl,
   customStyle,
+  isAttendee,
 }) {
+  const [copied, setCopied] = useState(false);
+
+  const handleShareClick = () => {
+    const currentUrl = `http://localhost:3000/event/${id}`;
+    navigator.clipboard.writeText(currentUrl).then(() => {
+      setCopied(true);
+      setTimeout(() => {
+        setCopied(false);
+      }, 2000);
+    });
+  };
+
   return (
     <Card
       component={Paper}
       elevation={2}
       sx={{
-        minWidth: {
-          xs: "70vw",
-          sm: "44vw",
-          md: "30vw",
-        },
         borderBottomLeftRadius: "4%",
         borderBottomRightRadius: "4%",
         transition: "transform 0.5s ease",
@@ -40,8 +55,7 @@ export default function EventCard({
       <CardMedia
         component="img"
         height="194"
-        image={Sideimage1}
-        alt="Paella dish"
+        image={`${import.meta.env.VITE_API_URL}/${imageUrl}`}
       />
       <CardContent>
         <Box
@@ -59,12 +73,20 @@ export default function EventCard({
             justifyContent="space-between"
             alignItems="center"
           >
-            <IconButton aria-label="add to favorites">
-              <FavoriteIcon />
-            </IconButton>
-            <IconButton aria-label="share">
-              <IosShare />
-            </IconButton>
+            {isAttendee && (
+              <IconButton>
+                <FavoriteIcon />
+              </IconButton>
+            )}
+            {!copied ? (
+              <IconButton onClick={handleShareClick}>
+                <IosShare />
+              </IconButton>
+            ) : (
+              <IconButton>
+                <DoneAll />
+              </IconButton>
+            )}
           </Box>
         </Box>
 
