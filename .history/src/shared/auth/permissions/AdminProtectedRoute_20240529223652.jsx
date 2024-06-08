46e025e48d289a706d05cbf961985a35ed60af99
ahmad anini <ahmadanini86@gmail.com>
2024-06-08 16:14:26 +0300

    delete history file

--- .history/src/shared/auth/permissions/AdminProtectedRoute_20240529223652.jsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { UserContext } from "../../../contexts/UserContext";
-
-export default function AdminProtectedRoute({ children }) {
-  const { isAdmin } = useContext(UserContext);
-  if (isAdmin) {
-    return <>{children}</>;
-  }
-}
