c8364a233bb69af9a0853f77fd252d6ec676fac7
Sameeh Hussein <sameehhussein02@gmail.com>
2024-06-13 18:03:37 +0300

    Handle No Event Case in Event near you

--- src/components/other/HomePageComponent/EventNearYou.jsx
+++ src/components/other/HomePageComponent/EventNearYou.jsx
@@ -59,67 +59,83 @@ export default function EventNearYou() {
   };
 
   return (
-    <Grid component="section" width="100%" maxWidth="90%" m="auto" mt={4}>
-      <Box
-        display="flex"
-        justifyContent="center"
-        flexWrap="wrap"
-        mb={3}
-        mt={4}
-        sx={{
-          gap: 2,
-        }}
-      >
-        <Swiper
-          slidesPerView={handelSlidesPerView()}
-          style={{
-            display: "flex",
-            flexDirection: "column-reverse",
-            marginBottom: 2,
-          }}
-        >
+    <>
+      {EventNearYou?.length === 0 ? (
+        <Grid component="section" width="100%" maxWidth="90%" m="auto" mt={4}>
+          <Typography variant="h5" sx={{ fontWeight: "bold", marginRight: 2 }}>
+            Event Near You
+          </Typography>
           <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            flexWrap="wrap"
+            height="40vh"
             sx={{
-              display: "flex",
-              alignItems: "center",
-              mb: 3,
+              gap: 2,
             }}
           >
-            <Typography variant="h5" sx={{ fontWeight: "bold", flexGrow: "1" }}>
-              Event Near You
+            <Typography variant="h6" sx={{ color: "gray" }}>
+              No events near you available at the moment.
             </Typography>
-
-            <SwiperButton />
           </Box>
+        </Grid>
+      ) : (
+        <Grid component="section" width="100%" maxWidth="90%" m="auto" mt={4}>
+          <Swiper
+            slidesPerView={handelSlidesPerView()}
+            style={{
+              display: "flex",
+              flexDirection: "column-reverse",
+              marginBottom: 2,
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                mb: 3,
+              }}
+            >
+              <Typography
+                variant="h5"
+                sx={{ fontWeight: "bold", flexGrow: "1" }}
+              >
+                Event Near You
+              </Typography>
 
-          <Box>
-            {EventNearYou?.map((event) => (
-              <SwiperSlide key={event.id}>
-                <EventCard
-                  key={event.id}
-                  id={event.id}
-                  imageUrl={event.thumbnailUrl}
-                  name={event.name}
-                  isOnline={event.isOnline}
-                  startDate={event.startDate}
-                  startTime={event.startTime}
-                  customStyle={cardStyle}
-                  isLikedByCurrentUser={event.isLikedByCurrentUser}
-                />
-              </SwiperSlide>
-            ))}
-            {EventNearLoading && (
-              <Box display="flex" gap={4}>
-                {Array.from(new Array(4)).map((_, index) => (
-                  <SwiperSlide key={index}>
-                    <EventCardLoading key={index} customStyle={cardStyle} />
-                  </SwiperSlide>
-                ))}
-              </Box>
-            )}
-          </Box>
-        </Swiper>
-      </Box>
-    </Grid>
+              <SwiperButton />
+            </Box>
+
+            <Box>
+              {EventNearYou?.map((event) => (
+                <SwiperSlide key={event.id}>
+                  <EventCard
+                    key={event.id}
+                    id={event.id}
+                    imageUrl={event.thumbnailUrl}
+                    name={event.name}
+                    isOnline={event.isOnline}
+                    startDate={event.startDate}
+                    startTime={event.startTime}
+                    customStyle={cardStyle}
+                    isLikedByCurrentUser={event.isLikedByCurrentUser}
+                  />
+                </SwiperSlide>
+              ))}
+              {EventNearLoading && (
+                <Box display="flex" gap={4}>
+                  {Array.from(new Array(4)).map((_, index) => (
+                    <SwiperSlide key={index}>
+                      <EventCardLoading key={index} customStyle={cardStyle} />
+                    </SwiperSlide>
+                  ))}
+                </Box>
+              )}
+            </Box>
+          </Swiper>
+        </Grid>
+      )}
+    </>
   );
 }
--- src/components/other/HomePageComponent/TopRatedEvent.jsx
+++ src/components/other/HomePageComponent/TopRatedEvent.jsx
@@ -65,73 +65,62 @@ export default function TopRatedEvent() {
         </Grid>
       ) : (
         <Grid component="section" width="100%" maxWidth="90%" m="auto" mt={4}>
-          <Box
-            display="flex"
-            justifyContent="center"
-            flexWrap="wrap"
-            mb={3}
-            mt={4}
-            sx={{
-              gap: 2,
+          <Swiper
+            slidesPerView={handelSlidesPerView()}
+            style={{
+              display: "flex",
+              flexDirection: "column-reverse",
+              marginBottom: 2,
             }}
           >
-            <Swiper
-              slidesPerView={handelSlidesPerView()}
-              style={{
+            <Box
+              sx={{
                 display: "flex",
-                flexDirection: "column-reverse",
-                marginBottom: 2,
+                alignItems: "center",
+                mb: 3,
               }}
             >
-              <Box
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  mb: 3,
-                }}
+              <Typography
+                variant="h5"
+                sx={{ fontWeight: "bold", flexGrow: "1" }}
               >
-                <Typography
-                  variant="h5"
-                  sx={{ fontWeight: "bold", flexGrow: "1" }}
-                >
-                  Top Rated Events
-                </Typography>
+                Top Rated Events
+              </Typography>
 
-                <SwiperButton />
-              </Box>
+              <SwiperButton />
+            </Box>
 
-              <Box>
-                {TopRatedEvents?.map((event) => {
-                  const rating = ratings[event.id] || 0;
-                  return (
-                    <SwiperSlide key={event.id}>
-                      <EventCard
-                        key={event.id}
-                        id={event.id}
-                        imageUrl={event.thumbnailUrl}
-                        name={event.name}
-                        isOnline={event.isOnline}
-                        startDate={event.startDate}
-                        startTime={event.startTime}
-                        customStyle={cardStyle}
-                        isLikedByCurrentUser={event.isLikedByCurrentUser}
-                        rating={rating}
-                      />
+            <Box>
+              {TopRatedEvents?.map((event) => {
+                const rating = ratings[event.id] || 0;
+                return (
+                  <SwiperSlide key={event.id}>
+                    <EventCard
+                      key={event.id}
+                      id={event.id}
+                      imageUrl={event.thumbnailUrl}
+                      name={event.name}
+                      isOnline={event.isOnline}
+                      startDate={event.startDate}
+                      startTime={event.startTime}
+                      customStyle={cardStyle}
+                      isLikedByCurrentUser={event.isLikedByCurrentUser}
+                      rating={rating}
+                    />
+                  </SwiperSlide>
+                );
+              })}
+              {TopRatedLoading && (
+                <Box display="flex" gap={4}>
+                  {Array.from(new Array(4)).map((_, index) => (
+                    <SwiperSlide key={index}>
+                      <EventCardLoading key={index} customStyle={cardStyle} />
                     </SwiperSlide>
-                  );
-                })}
-                {TopRatedLoading && (
-                  <Box display="flex" gap={4}>
-                    {Array.from(new Array(4)).map((_, index) => (
-                      <SwiperSlide key={index}>
-                        <EventCardLoading key={index} customStyle={cardStyle} />
-                      </SwiperSlide>
-                    ))}
-                  </Box>
-                )}
-              </Box>
-            </Swiper>
-          </Box>
+                  ))}
+                </Box>
+              )}
+            </Box>
+          </Swiper>
         </Grid>
       )}
     </>
