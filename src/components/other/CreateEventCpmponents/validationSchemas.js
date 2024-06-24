7e6c54d3c12bedcae5ad17f9bfeac8fbec2c1bce
ahmad anini <ahmadanini86@gmail.com>
2024-06-24 16:54:46 +0300

    resolve conflict

--- src/components/other/CreateEventCpmponents/validationSchemas.js
+++ src/components/other/CreateEventCpmponents/validationSchemas.js
@@ -156,13 +156,6 @@ export const validationSchemaStepTwo = yup.object({
           .min(
             dayjs().subtract(1, "day"),
             "Start Sale date must be in the future"
-<<<<<<< HEAD
-=======
-          )
-          .max(
-            yup.ref("$startDate"),
-            "start Sale date must be after the main event start date"
->>>>>>> 083fb72e1e0bf7de6b809f57a0dabfef918fcef2
           ),
         endSale: yup
           .date()
@@ -172,10 +165,6 @@ export const validationSchemaStepTwo = yup.object({
             dayjs().subtract(1, "day"),
             "End Sale date must be in the future"
           )
-<<<<<<< HEAD
-=======
-          .max(yup.ref("$endDate"), "End Sale date must be before the main event end date")
->>>>>>> 083fb72e1e0bf7de6b809f57a0dabfef918fcef2
           .min(yup.ref("startSale"), "End Sale date must be after start sale"),
         startSaleTime: yup
           .date()
