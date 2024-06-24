47fbef77e1565cb21e9fd4955c8b96c58981d42f
SaifAbuR3d <saefraed2@gmail.com>
2024-06-24 13:47:32 +0300

    delete github action workflow

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -16,11 +16,11 @@ export function UserContextProvider({ children }) {
       id: tokenData["id"],
       userId:
         tokenData[
-          "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"
+        "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"
         ],
       email:
         tokenData[
-          "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"
+        "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"
         ],
       role: tokenData[
         "http://schemas.microsoft.com/ws/2008/06/identity/claims/role"
